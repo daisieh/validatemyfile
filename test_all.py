e503f8844697acd901d6576c8e83cb38b019b480
daisie_local <daisieh@gmail.com>
2017-08-04 15:06:45 -0700

    add negative tests

--- test_all.py
+++ test_all.py
@@ -4,12 +4,19 @@ import sys
 import re
 
 def test_all():
-	files = os.listdir('test')
-	for file in files:
-		if re.match("^\.", file) is None:
-			print("checking " + file)
-			assert validate.validate(os.path.join('test',file)), "%s is INVALID FILE FORMAT" % file
-
+    files = os.listdir('test')
+    for file in files:
+        if re.match("^\.", file) is None:
+            print("checking " + file)
+            assert validate.validate(os.path.join('test',file)), "%s is INVALID FILE FORMAT" % file
+    
+    filetypes = os.listdir('filetypes')
+    filename = os.path.join('test','test.vcf')
+    for filetype in filetypes:
+        extension = re.match("^([a-zA-Z0-9].+?)\.py$", filetype)
+        if extension is not None and extension.group(1) != 'vcf':
+            module = __import__("filetypes." + extension.group(1), globals(), locals(), ['check', 'description'])
+            assert (module.check(filename) == False), "%s fails to reject invalid file" % filetype
 if __name__ == '__main__':
     test_all()
 
